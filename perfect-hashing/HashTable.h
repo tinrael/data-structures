6a6aff13e19c98af70c09707c08d154de6bc6d13
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-01 22:10:19 +0200

    Added the HashFunction data member to the HashTable class

--- perfect-hashing/HashTable.h
+++ perfect-hashing/HashTable.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "HashFunction.h"
 #include <cstddef>
 
 template <typename T>
@@ -9,13 +10,15 @@ private:
 	T* table;
 	size_t size;
 
+	HashFunction hashFunction;
+
 public:
 	HashTable(std::size_t size);
 	~HashTable();
 };
 
 template<typename T>
-inline HashTable<T>::HashTable(std::size_t size) : size(size)
+inline HashTable<T>::HashTable(std::size_t size) : size(size), hashFunction(size)
 {
 	table = new T[size];
 }
