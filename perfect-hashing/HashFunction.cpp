6a5cfa08ff0f422e7cd9a3e354afe6211d6bf588
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-01 20:49:05 +0200

    Added the result type (unsigned) to the uniform_int_distribution

--- perfect-hashing/HashFunction.cpp
+++ perfect-hashing/HashFunction.cpp
@@ -4,12 +4,12 @@
 HashFunction::HashFunction(unsigned m) : m(m)
 {
 	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis1(1, p - 1);
-	std::uniform_int_distribution<> dis2(0, p - 1);
-	
-	a = dis1(gen);
-	b = dis2(gen);
+	std::mt19937 generator(rd());
+	std::uniform_int_distribution<unsigned> dis1(1, p - 1);
+	std::uniform_int_distribution<unsigned> dis2(0, p - 1);
+
+	a = dis1(generator);
+	b = dis2(generator);
 }
 
 // TODO: Add hash function from universal class of hash functions here.
