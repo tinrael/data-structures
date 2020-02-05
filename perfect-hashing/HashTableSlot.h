612109e2b47fedf251aff6ed05387c19bc9851b7
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-05 17:48:35 +0200

    Added the HashTableSlot class

---
+++ perfect-hashing/HashTableSlot.h
@@ -0,0 +1,39 @@
+#pragma once
+
+template<typename T, typename U>
+class HashTableSlot
+{
+private:
+	T key;
+	U *data;
+
+public:
+	HashTableSlot(T key, U *data);
+	~HashTableSlot();
+
+	T getKey();
+	U* getData();
+};
+
+template<typename T, typename U>
+inline HashTableSlot<T, U>::HashTableSlot(T key, U *data) : key(key), data(data)
+{
+}
+
+template<typename T, typename U>
+inline HashTableSlot<T, U>::~HashTableSlot()
+{
+	delete data;
+}
+
+template<typename T, typename U>
+inline T HashTableSlot<T, U>::getKey()
+{
+	return key;
+}
+
+template<typename T, typename U>
+inline U* HashTableSlot<T, U>::getData()
+{
+	return data;
+}
