f8c6aaf32fe7f3e1dafe8cf924898630d2abf438
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-08 22:34:22 +0200

    Changed the table variable type; added data members, the initializer list and the destructor

--- perfect-hashing/PerfectHashTable.h
+++ perfect-hashing/PerfectHashTable.h
@@ -1,22 +1,43 @@
 #pragma once
 
 #include "HashTable.h"
+#include "HashFunction.h"
 #include "HashTableSlot.h"
+#include <cstddef>
+#include <vector>
 
 template<typename KeyType, typename DataType>
 class PerfectHashTable
 {
 private:
-	HashTable<KeyType, HashTable<KeyType, DataType>> table;
+	HashTable<KeyType, DataType>** primaryTable;
+	std::size_t size;
+
+	HashFunction hashFunction;
 
 public:
 	PerfectHashTable(const std::vector<HashTableSlot<KeyType, DataType>*>& slots);
+	~PerfectHashTable();
+
 	DataType* search(KeyType key);
 };
 
 template<typename KeyType, typename DataType>
-inline PerfectHashTable<KeyType, DataType>::PerfectHashTable(const std::vector<HashTableSlot<KeyType, DataType>*>& slots)
+inline PerfectHashTable<KeyType, DataType>::PerfectHashTable(const std::vector<HashTableSlot<KeyType, DataType>*>& slots) : 
+	primaryTable(new HashTable<KeyType, DataType>* [slots.size()]()),
+	size(slots.size()),
+	hashFunction(slots.size())
 {
+}
+
+// TODO: Use smart pointers.
+template<typename KeyType, typename DataType>
+inline PerfectHashTable<KeyType, DataType>::~PerfectHashTable()
+{
+	for (std::size_t i = 0; i < size; i++) {
+		delete primaryTable[i];
+	}
+	delete[] primaryTable;
 }
 
 template<typename KeyType, typename DataType>
