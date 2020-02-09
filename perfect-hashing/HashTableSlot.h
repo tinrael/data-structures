a9ab55a0a89dc3b10c9a8e6fd85404b0b55fd78b
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-09 20:19:23 +0200

    Deleted the HashTableSlot destructor

--- perfect-hashing/HashTableSlot.h
+++ perfect-hashing/HashTableSlot.h
@@ -5,26 +5,18 @@ class HashTableSlot
 {
 private:
 	KeyType key;
-	DataType *data;
+	DataType* data; // TODO: Use smart pointers.
 
 public:
-	HashTableSlot(KeyType key, DataType *data);
-	~HashTableSlot();
+	HashTableSlot(KeyType key, DataType* data);
 
 	KeyType getKey();
 	DataType* getData();
 };
 
 template<typename KeyType, typename DataType>
-inline HashTableSlot<KeyType, DataType>::HashTableSlot(KeyType key, DataType *data) : key(key), data(data)
+inline HashTableSlot<KeyType, DataType>::HashTableSlot(KeyType key, DataType* data) : key(key), data(data)
 {
-}
-
-// TODO: Use smart pointers. Are you sure to delete data?
-template<typename KeyType, typename DataType>
-inline HashTableSlot<KeyType, DataType>::~HashTableSlot()
-{
-	delete data;
 }
 
 template<typename KeyType, typename DataType>
