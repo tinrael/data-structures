6885cd2b153838b9aea6c6f5d9235daf2b4f671f
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-12 21:20:11 +0200

    Added new comment

--- perfect-hashing/HashTableSlot.h
+++ perfect-hashing/HashTableSlot.h
@@ -5,7 +5,7 @@ class HashTableSlot
 {
 private:
 	KeyType key;
-	DataType* data; // TODO: Use smart pointers.
+	DataType* data;
 
 public:
 	HashTableSlot(KeyType key, DataType* data);
@@ -14,6 +14,7 @@ public:
 	DataType* getData();
 };
 
+// The slot does not take ownership of data.
 template<typename KeyType, typename DataType>
 inline HashTableSlot<KeyType, DataType>::HashTableSlot(KeyType key, DataType* data) : key(key), data(data)
 {
