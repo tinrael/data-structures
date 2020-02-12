631bf6030ba03063e945a42f4251280ae733ce87
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-12 23:09:55 +0200

    Added additional information to the console output

--- perfect-hashing/perfect-hashing.cpp
+++ perfect-hashing/perfect-hashing.cpp
@@ -11,8 +11,7 @@
 int main()
 {
 	std::ifstream inputFile;
-	inputFile.open("countries.txt");
-	
+	inputFile.open("countries.txt");	
 	if (!inputFile.is_open()) {
 		std::cout << "Error. Failed to open the file." << std::endl;
 		return 1;
@@ -30,24 +29,43 @@ int main()
 	inputFile.close();
 	
 	PerfectHashTable<std::string, Country>* table = new PerfectHashTable<std::string, Country>(slots);
+	
+	std::cout << "-----" << std::endl;
+	std::cout << "| This application is using perfect hashing to store the static set of countries from the file 'countries.txt'." << std::endl;
+	std::cout << "| The searching takes constant time O(1) in the worst case." << std::endl;
+	std::cout << "-----" << std::endl;
+
+	std::cout << "| The figure below represents the two levels of hashing." << std::endl;
+	std::cout << "-----" << std::endl;
+	
 	table->printKeys();
 	
+	std::cout << "-----" << std::endl;
+	std::cout << "| Searching: " << std::endl;
+	std::cout << "-----" << std::endl;
+	
+	Country* country = nullptr;
 	for (std::size_t i = 0; i < 10; i++) {
-		std::cout << "Enter the country: ";
+		std::cout << "Enter the country to search: ";	
 		std::cin >> name;
-		Country* country = table->search(name);
+
+		country = table->search(name);
+		
+		std::cout << "-----" << std::endl;
 		if (country) {
 			std::cout << *country;
 		}
 		else {
 			std::cout << "Not found." << std::endl;
 		}
+		std::cout << "-----" << std::endl;
 	}
 
 	for (HashTableSlot<std::string, Country>* slot : slots) {
 		delete slot->getData();
 		delete slot;
 	}
+	slots.clear();
 	delete table;
 
 	return 0;
