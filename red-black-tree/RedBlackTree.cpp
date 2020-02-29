2cd18ad273f7ce3e2f7d30ac10b20b8dca24a5f8
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-02-29 22:13:06 +0200

    Add the RBTreeNode class

--- red-black-tree/RedBlackTree.cpp
+++ red-black-tree/RedBlackTree.cpp
@@ -1,1 +1,5 @@
 #include "RedBlackTree.h"
+
+RBTreeNode::RBTreeNode(int key) : key(key), color(COLOR_RED), parent(nullptr), left(nullptr), right(nullptr)
+{
+}
--- red-black-tree/RedBlackTree.h
+++ red-black-tree/RedBlackTree.h
@@ -1,4 +1,23 @@
 #pragma once
+
+enum Color {
+	COLOR_RED,
+	COLOR_BLACK
+};
+
+// Red-black Tree Node
+class RBTreeNode {
+public:
+	int key;
+	Color color;
+
+	RBTreeNode* parent;
+	RBTreeNode* left;
+	RBTreeNode* right;
+
+	RBTreeNode(int key);
+};
+
 class RedBlackTree
 {
 };
