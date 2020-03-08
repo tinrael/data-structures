efec0773a3ce929f7724660efd5b7d9f23a42277
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-03-08 15:15:38 +0200

    Add example of usage

--- red-black-tree/red-black-tree.cpp
+++ red-black-tree/red-black-tree.cpp
@@ -1,20 +1,26 @@
-// red-black-tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "RedBlackTree.h"
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::ofstream outputFile("graph1.gv");
+	if (!outputFile.is_open()) {
+		std::cout << "Unable to open file." << std::endl;
+		return 1;
+	}
+
+	RedBlackTree tree;
+	tree.insert(6);
+	tree.insert(17);
+	tree.insert(34);
+	tree.insert(56);
+	tree.insert(112);
+	tree.insert(2);
+	tree.insert(1);
+	tree.insert(9);
+	tree.print();
+
+	// Prints the tree in the DOT language to the graph1.gv file.
+	tree.printDotLanguage(outputFile);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
