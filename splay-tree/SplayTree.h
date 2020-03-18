438f923b4a489551dbaec763f34ddfa24d9a3738
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-03-18 15:29:34 +0200

    Add the TreeNode class

--- splay-tree/SplayTree.h
+++ splay-tree/SplayTree.h
@@ -1,5 +1,22 @@
 #pragma once
+
+template <typename T>
+class TreeNode {
+public:
+	T key;
+
+	TreeNode<T>* parent;
+	TreeNode<T>* left;
+	TreeNode<T>* right;
+
+	TreeNode(T key);
+};
+
 class SplayTree
 {
 };
 
+template<typename T>
+inline TreeNode<T>::TreeNode(T key) : key(key), parent(nullptr), left(nullptr), right(nullptr)
+{
+}
