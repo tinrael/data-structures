1950bc273cb5d442dd59cd5b0cd86d054268591f
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-03-18 15:34:33 +0200

    Add left and right rotations

--- splay-tree/SplayTree.h
+++ splay-tree/SplayTree.h
@@ -12,11 +12,78 @@ public:
 	TreeNode(T key);
 };
 
+template <typename T>
 class SplayTree
 {
+private:
+	TreeNode<T>* root;
+
+	void rotateLeft(TreeNode<T>* x);
+	void rotateRight(TreeNode<T>* y);
+
+public:
+	SplayTree();
+
 };
 
 template<typename T>
 inline TreeNode<T>::TreeNode(T key) : key(key), parent(nullptr), left(nullptr), right(nullptr)
+{
+}
+
+template<typename T>
+inline void SplayTree<T>::rotateLeft(TreeNode<T>* x)
+{
+	if (!x || !x->right) {
+		return;
+	}
+
+	TreeNode<T>* y = x->right;
+	x->right = y->left;
+	if (y->left) {
+		y->left->parent = x;
+	}
+	y->parent = x->parent;
+	if (!x->parent) {
+		this->root = y;
+	}
+	else if (x == x->parent->left) {
+		x->parent->left = y;
+	}
+	else {
+		x->parent->right = y;
+	}
+	y->left = x;
+	x->parent = y;
+}
+
+template<typename T>
+inline void SplayTree<T>::rotateRight(TreeNode<T>* y)
+{
+	if (!y || !y->left) {
+		return;
+	}
+
+	TreeNode<T>* x = y->left;
+	y->left = x->right;
+	if (x->right) {
+		x->right->parent = y;
+	}
+	x->parent = y->parent;
+	if (!y->parent) {
+		this->root = x;
+	}
+	else if (y == y->parent->right) {
+		y->parent->right = x;
+	}
+	else {
+		y->parent->left = x;
+	}
+	x->right = y;
+	y->parent = x;
+}
+
+template<typename T>
+inline SplayTree<T>::SplayTree() : root(nullptr)
 {
 }
