5780d221c5f1c8ce16cee03e31cffd30c3b8f0f8
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-04-18 16:30:59 +0300

    Add a new example of usage: keys from the SQLite database

--- optimal-binary-search-tree/optimal-binary-search-tree.cpp
+++ optimal-binary-search-tree/optimal-binary-search-tree.cpp
@@ -1,25 +1,54 @@
 #include "OptimalBinarySearchTree.h"
-
+#include "sqlite3.h"
 #include <iostream>
 #include <fstream>
-#include <cstddef>
+#include <string>
+#include <vector>
+
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+	if (argv[0]) {
+		std::vector<std::string>* keys = static_cast<std::vector<std::string>*>(NotUsed);
+		keys->push_back(argv[0]);
+	}
+	return 0;
+}
 
 int main()
 {
+	sqlite3* db;
+	char* zErrMsg = 0;
+	int rc = sqlite3_open("countries.db", &db);
+	if (rc) {
+		std::cout << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+		sqlite3_close(db);
+		return(1);
+	}
+
 	std::ofstream outputFile("graph1.gv");
 	if (!outputFile.is_open()) {
 		std::cout << "Unable to open file." << std::endl;
 		return 1;
 	}
 
-	int keys[] = { 10, 14, 17, 20 };
-	double probabilities[] = { 0.3, 0.1, 0.4, 0.2 };
-	std::size_t size = sizeof(keys) / sizeof(keys[0]);
+	const char* sql = "select name from country order by name asc limit 5;";
 
-	OptimalBinarySearchTree<int> tree(keys, probabilities, size);
+	std::vector<std::string> keys;
+	double probabilities[] = { 0.05, 0.3, 0.15, 0.2, 0.3 };
 
-	std::cout << "Expected cost of searching an optimal BST: " << tree.getExpectedSearchCost() << std::endl;
- 
-	// Prints the tree in the DOT language to the graph1.gv file.
-	tree.printDotLanguage(outputFile);
+	rc = sqlite3_exec(db, sql, callback, &keys, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		std::cout << "SQL error: " << zErrMsg << std::endl;
+		sqlite3_free(zErrMsg);
+	}
+	else {
+		OptimalBinarySearchTree<std::string> tree(&keys[0], probabilities, keys.size());
+		std::cout << "Expected cost of searching an optimal BST: " << tree.getExpectedSearchCost() << std::endl;
+		// Prints the tree in the DOT language to the graph1.gv file.
+		tree.printDotLanguage(outputFile);
+	}
+
+	outputFile.close();
+	sqlite3_close(db);
+
+	return 0;
 }
