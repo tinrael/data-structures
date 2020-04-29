02083e880ea46099672d0d9ff2b8f8e45d89d40d
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-04-29 16:51:33 +0300

    Implemented the insert operation

--- fibonacci-heap/FibonacciHeap.h
+++ fibonacci-heap/FibonacciHeap.h
@@ -7,15 +7,38 @@ template <typename T>
 class FibonacciHeap
 {
 private:
-	std::size_t n; // number of nodes in the Fibonacci heap
+	std::size_t numOfNodes; // number of nodes in the Fibonacci heap
 	Node<T>* min;
 
 public:
 	FibonacciHeap();
+	
+	void insert(T key);
 
 };
 
 template<typename T>
-inline FibonacciHeap<T>::FibonacciHeap() : n(0), min(nullptr)
+inline FibonacciHeap<T>::FibonacciHeap() : numOfNodes(0), min(nullptr)
 {
+}
+
+template<typename T>
+inline void FibonacciHeap<T>::insert(T key)
+{
+	// The Fibonacci heap tree ownership of x.
+	Node<T>* x = new Node<T>(key);
+	if (!min) {
+		min = x;
+	}
+	else {
+		x->right = min;
+		x->left = min->left;
+		min->left->right = x;
+		min->left = x;
+		
+		if (x->key < min->key) {
+			min = x;
+		}
+	}
+	numOfNodes++;
 }
