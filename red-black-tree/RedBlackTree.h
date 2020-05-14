44ea057438aa571221a1f59bf73e65fc427e62a1
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-05-14 18:30:07 +0300

    Add templates to the red-black-tree project
    Add templates to the RedBlackTree class and to the RBTreeNode class.
    Replace the RBTreeNode class into separate .h file.

--- red-black-tree/RedBlackTree.h
+++ red-black-tree/RedBlackTree.h
@@ -1,50 +1,31 @@
 #pragma once
 
+#include "RBTreeNode.h"
 #include <iostream>
-#include <cstddef>
+#include <stdexcept>
 
-enum Color {
-	COLOR_RED,
-	COLOR_BLACK
-};
-
-// Red-black Tree Node
-class RBTreeNode {
-public:
-	static std::size_t idGenerator;
-	std::size_t id;
-	
-	int key;
-	Color color;
-
-	RBTreeNode* parent;
-	RBTreeNode* left;
-	RBTreeNode* right;
-
-	RBTreeNode(int key);
-};
-
+template <typename KeyType>
 class RedBlackTree
 {
 private:
-	RBTreeNode* root;
+	RBTreeNode<KeyType>* root;
 
-	void fixup(RBTreeNode* z);
-	void rotateLeft(RBTreeNode* x);
-	void rotateRight(RBTreeNode* y);
+	void fixup(RBTreeNode<KeyType>* z);
+	void rotateLeft(RBTreeNode<KeyType>* x);
+	void rotateRight(RBTreeNode<KeyType>* y);
 
 	// Prints the tree according to the inorder traversal.
-	void print(const RBTreeNode* tree, std::ostream& out);
-	void deleteRBTreeNode(RBTreeNode* node);
+	void print(const RBTreeNode<KeyType>* tree, std::ostream& out);
+	void deleteRBTreeNode(RBTreeNode<KeyType>* node);
 
-	void printDotVertices(RBTreeNode* tree, std::ostream& out);
-	void printDotEdges(RBTreeNode* tree, std::ostream& out);
+	void printDotVertices(RBTreeNode<KeyType>* tree, std::ostream& out);
+	void printDotEdges(RBTreeNode<KeyType>* tree, std::ostream& out);
 
 public:
 	RedBlackTree();
 	~RedBlackTree();
 
-	void insert(int key);
+	void insert(KeyType key);
 	// Prints the tree according to the inorder traversal.
 	void print(std::ostream& out = std::cout);
 	// Deletes all nodes.
@@ -53,3 +34,232 @@ public:
 	// Prints the tree in the DOT language.
 	void printDotLanguage(std::ostream& out = std::cout);
 };
+
+template <typename KeyType>
+inline void RedBlackTree<KeyType>::fixup(RBTreeNode<KeyType>* z)
+{
+	if (!z) {
+		throw std::invalid_argument("The argument z is nullptr.");
+	}
+	while (z->parent && z->parent->color == COLOR_RED) {
+		if (z->parent == z->parent->parent->left) {
+			RBTreeNode<KeyType>* y = z->parent->parent->right;
+			if (y && y->color == COLOR_RED) {
+				z->parent->color = COLOR_BLACK;
+				y->color = COLOR_BLACK;
+				z->parent->parent->color = COLOR_RED;
+				z = z->parent->parent;
+			}
+			else {
+				if (z == z->parent->right) {
+					z = z->parent;
+					rotateLeft(z);
+				}
+				z->parent->color = COLOR_BLACK;
+				z->parent->parent->color = COLOR_RED;
+				rotateRight(z->parent->parent);
+			}
+		}
+		else {
+			RBTreeNode<KeyType>* y = z->parent->parent->left;
+			if (y && y->color == COLOR_RED) {
+				z->parent->color = COLOR_BLACK;
+				y->color = COLOR_BLACK;
+				z->parent->parent->color = COLOR_RED;
+				z = z->parent->parent;
+			}
+			else {
+				if (z == z->parent->left) {
+					z = z->parent;
+					rotateRight(z);
+				}
+				z->parent->color = COLOR_BLACK;
+				z->parent->parent->color = COLOR_RED;
+				rotateLeft(z->parent->parent);
+			}
+		}
+	}
+	this->root->color = COLOR_BLACK;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::rotateLeft(RBTreeNode<KeyType>* x)
+{
+	if (!x || !x->right) {
+		return;
+	}
+
+	RBTreeNode<KeyType>* y = x->right;
+	x->right = y->left;
+	if (y->left) {
+		y->left->parent = x;
+	}
+	y->parent = x->parent;
+	if (!x->parent) {
+		this->root = y;
+	}
+	else if (x == x->parent->left) {
+		x->parent->left = y;
+	}
+	else {
+		x->parent->right = y;
+	}
+	y->left = x;
+	x->parent = y;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::rotateRight(RBTreeNode<KeyType>* y)
+{
+	if (!y || !y->left) {
+		return;
+	}
+
+	RBTreeNode<KeyType>* x = y->left;
+	y->left = x->right;
+	if (x->right) {
+		x->right->parent = y;
+	}
+	x->parent = y->parent;
+	if (!y->parent) {
+		this->root = x;
+	}
+	else if (y == y->parent->right) {
+		y->parent->right = x;
+	}
+	else {
+		y->parent->left = x;
+	}
+	x->right = y;
+	y->parent = x;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::print(const RBTreeNode<KeyType>* tree, std::ostream& out)
+{
+	if (!tree) {
+		return;
+	}
+	print(tree->left, out);
+	out << tree->key << " ";
+	print(tree->right, out);
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::deleteRBTreeNode(RBTreeNode<KeyType>* node)
+{
+	if (!node) {
+		return;
+	}
+	deleteRBTreeNode(node->left);
+	deleteRBTreeNode(node->right);
+	delete node;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::printDotVertices(RBTreeNode<KeyType>* tree, std::ostream& out)
+{
+	if (!tree) {
+		return;
+	}
+	out << tree->id << " [label=\"" << tree->key;
+	switch (tree->color) {
+	case COLOR_BLACK:
+		out << "\", fillcolor=black]" << std::endl;
+		break;
+	case COLOR_RED:
+		out << "\", fillcolor=red]" << std::endl;
+		break;
+	default:
+		out << "\"]" << std::endl;
+		break;
+	}
+	printDotVertices(tree->left, out);
+	printDotVertices(tree->right, out);
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::printDotEdges(RBTreeNode<KeyType>* tree, std::ostream& out)
+{
+	if (!tree) {
+		return;
+	}
+	if (tree->left) {
+		out << tree->id << " -> ";
+		printDotEdges(tree->left, out);
+	}
+	if (tree->right) {
+		out << tree->id << " -> ";
+		printDotEdges(tree->right, out);
+	}
+	if (!tree->right && !tree->left) {
+		out << tree->id << std::endl;
+	}
+}
+
+template<typename KeyType>
+inline RedBlackTree<KeyType>::RedBlackTree() : root(nullptr)
+{
+}
+
+template<typename KeyType>
+inline RedBlackTree<KeyType>::~RedBlackTree()
+{
+	clear();
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::insert(KeyType key)
+{
+	// The red-black tree takes ownership of z.
+	RBTreeNode<KeyType>* z = new RBTreeNode<KeyType>(key);
+	RBTreeNode<KeyType>* x = this->root;
+	RBTreeNode<KeyType>* y = nullptr;
+	while (x) {
+		y = x;
+		if (z->key < x->key) {
+			x = x->left;
+		}
+		else {
+			x = x->right;
+		}
+	}
+	z->parent = y;
+	if (!y) {
+		this->root = z;
+	}
+	else if (z->key < y->key) {
+		y->left = z;
+	}
+	else {
+		y->right = z;
+	}
+	fixup(z);
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::print(std::ostream& out)
+{
+	print(this->root, out);
+	out << std::endl;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::printDotLanguage(std::ostream& out)
+{
+	if (!this->root) {
+		return;
+	}
+	out << "digraph G {" << std::endl;
+	out << "node[style=filled, fontname=Helvetica, fontcolor=white, fontsize=20]" << std::endl;
+	printDotVertices(this->root, out);
+	printDotEdges(this->root, out);
+	out << "}" << std::endl;
+}
+
+template<typename KeyType>
+inline void RedBlackTree<KeyType>::clear()
+{
+	deleteRBTreeNode(this->root);
+	this->root = nullptr;
+}
